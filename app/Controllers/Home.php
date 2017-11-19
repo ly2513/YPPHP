c671ad02c82bcaca1b9fa84b6a9b6ee66675a827
ly2513 <626375290@qq.com>
2017-11-19 23:43:15 +0800

    修复redis操作session bug

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -27,8 +27,6 @@ class Home extends Controller
      */
     public function index()
     {
-        $_SESSION['id'] = 2004;
-        $_SESSION['name'] = 'liyong';
         $time         = microtime(true) * 1000;
         $elapsed_time = number_format(($time - START_TIME), 0);
         $this->assign('view_path', 'app/Views/' . $this->controller . '/' . $this->method . $this->extension);
